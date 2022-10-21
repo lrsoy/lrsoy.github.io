157aef5bc7b4b476adbeeb7a10c8cec258bbea3a
lrsoy <lying_jk_bp@163.com>
2022-10-21 23:44:52 +0800

    Configure the plugin，auto import and auto components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(() => { // { command, mode }
+  return {
+    plugins: [
+      vue({
+        include: [/\.vue$/],
+        reactivityTransform: true,
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+        ],
+      }),
+      Components({
+        extensions: ['vue', 'tsx'],
+        dts: true,
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dirs: ['src/components'],
+        directoryAsNamespace: true,
+      }),
+    ],
+  }
 })
