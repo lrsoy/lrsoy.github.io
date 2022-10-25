d26f5030076d23c060e8f87e12e2ad52be598b7f
lrsoy <lying_jk_bp@163.com>
2022-10-25 23:54:09 +0800

    configure about Markdown

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import fs from 'fs-extra'
+import Pages from 'vite-plugin-pages'
+import matter from 'gray-matter'
+
+import Markdown from 'vite-plugin-vue-markdown'
+import Shiki from 'markdown-it-shiki'
+import anchor from 'markdown-it-anchor'
+import LinkAttributes from 'markdown-it-link-attributes'
+import { slugify } from './scripts/slugify'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => { // { command, mode }
@@ -15,9 +24,18 @@ export default defineConfig(() => { // { command, mode }
         },
       ],
     },
+    optimizeDeps: {
+      include: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+        'dayjs',
+        'dayjs/plugin/localizedFormat',
+      ],
+    },
     plugins: [
       vue({
-        include: [/\.vue$/],
+        include: [/\.vue$/, /\.md$/],
         reactivityTransform: true,
       }),
       AutoImport({
@@ -33,6 +51,70 @@ export default defineConfig(() => { // { command, mode }
         dirs: ['src/components'],
         directoryAsNamespace: true,
       }),
+
+      Pages({  // vite-plugin-pages 的配置
+        extensions: ['vue', 'md'],
+        pagesDir: 'pages',
+        extendRoute(route) {
+          const path = resolve(__dirname, route.component.slice(1))
+
+          if (!path.includes('projects.md')) {
+            const md = fs.readFileSync(path, 'utf-8')
+            const { data } = matter(md)
+            route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+          }
+
+          return route
+        },
+      }),
+
+
+      Markdown({
+        wrapperComponent: 'post',
+        wrapperClasses: 'prose m-auto',
+        headEnabled: true,
+        markdownItOptions: {
+          quotes: '""\'\'',
+        },
+        markdownItSetup(md) {
+          md.use(Shiki, {
+            theme: {
+              light: 'vitesse-light',
+              dark: 'vitesse-dark',
+            },
+          })
+          md.use(anchor, {
+            slugify,
+            permalink: anchor.permalink.linkInsideHeader({
+              symbol: '#',
+              renderAttrs: () => ({ 'aria-hidden': 'true' }),
+            }),
+          })
+
+          md.use(LinkAttributes, {
+            matcher: (link: string) => /^https?:\/\//.test(link),
+            attrs: {
+              target: '_blank',
+              rel: 'noopener',
+            },
+          })
+        },
+      }),
     ],
+
+    build: {
+      rollupOptions: {
+        onwarn(warning, next) {
+          if (warning.code !== 'UNUSED_EXTERNAL_IMPORT')
+            next(warning)
+        },
+      },
+    },
+
+    ssgOptions: {
+      formatting: 'minify',
+      format: 'cjs',
+    },
+
   }
 })
