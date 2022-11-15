4d5bd75b9a181703c8300ebd608a7fd399f7e7eb
lrsoy <lying_jk_bp@163.com>
2022-11-15 21:28:46 +0800

    给内容添加滚动

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,8 +1,8 @@
 <!-- Post 内容区域，具体怎么实现的还在考察 -->
 
 <script setup lang="ts">
-import { useSessionStorage, useStorage } from '@vueuse/core'
-import { computed, onUnmounted, ref, watch } from 'vue'
+import { useSessionStorage, useStorage, useEventListener } from '@vueuse/core'
+import { computed, onUnmounted, onMounted, ref, watch } from 'vue'
 import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router'
 import { formatDate } from '~/logics'
 const { frontmatter } = defineProps({
@@ -31,6 +31,56 @@ const link = computed(() => {
   }
   return i
 })
+
+
+onMounted(() => {
+  const navigate = () => {
+    if (location.hash) {
+      document.querySelector(decodeURIComponent(location.hash))
+        ?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  const handleAnchors = (
+    event: MouseEvent & { target: HTMLElement },
+  ) => {
+    const link = event.target.closest('a')
+
+    if (
+      !event.defaultPrevented
+      && link
+      && event.button === 0
+      && link.target !== '_blank'
+      && link.rel !== 'external'
+      && !link.download
+      && !event.metaKey
+      && !event.ctrlKey
+      && !event.shiftKey
+      && !event.altKey
+    ) {
+      const url = new URL(link.href)
+      if (url.origin !== window.location.origin)
+        return
+
+      event.preventDefault()
+      const { pathname, hash } = url
+      if (hash && (!pathname || pathname === location.pathname)) {
+        window.history.replaceState({}, '', hash)
+        navigate()
+      }
+      else {
+        router.push({ path: pathname, hash })
+      }
+    }
+  }
+
+  useEventListener(window, 'hashchange', navigate)
+  useEventListener(content.value!, 'click', handleAnchors, { passive: false })
+
+  navigate()
+  setTimeout(navigate, 500)
+})
+
 </script>
 
 <template>
